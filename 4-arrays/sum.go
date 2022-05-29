68f0deae77f0d3065a3e82a0f990353ab6cef255
Tilmann Seiffert <mail@tilseiffert.de>
2022-05-29 15:33:34 +0200

    new chapter arrays

---
+++ 4-arrays/sum.go
@@ -0,0 +1,10 @@
+package arrays
+
+func Sum(numbers [5]int) (sum int) {
+
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+
+	return
+}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/tilseiffert/golang-learn-with-tdd
-
-go 1.18
