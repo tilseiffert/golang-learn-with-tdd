5c5d2a1457c06aa55bc743175c26b3c75ddb3059
Tilmann Seiffert <mail@tilseiffert.de>
2022-05-29 15:45:13 +0200

    test refactoring

--- 4-arrays/sum_test.go
+++ 4-arrays/sum_test.go
@@ -15,15 +15,16 @@ func TestSum(t *testing.T) {
 		}
 	})
 
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
+	// t.Run("collection of any size", func(t *testing.T) {
+	// 	numbers := []int{1, 2, 3}
 
-		got := Sum(numbers)
-		want := 6
+	// 	got := Sum(numbers)
+	// 	want := 6
 
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
+	// 	if got != want {
+	// 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	// 	}
+	// })
+	//// already coverd by previous test
 
 }
