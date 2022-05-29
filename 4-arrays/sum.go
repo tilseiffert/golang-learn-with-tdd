474f2727c1a4fb6f09ef3b8952466ae44c93d9f9
Tilmann Seiffert <mail@tilseiffert.de>
2022-05-29 15:57:41 +0200

    SummAllTails()

--- 4-arrays/sum.go
+++ 4-arrays/sum.go
@@ -8,11 +8,19 @@ func Sum(numbers []int) (sum int) {
 }
 
 func SumAll(numbersToSum ...[]int) (sums []int) {
-	lengthOfNumbers := len(numbersToSum)
-	sums = make([]int, lengthOfNumbers)
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
 	}
 
 	return
