310886a9b1cd16dcc33a2d8702675ae054fda17c
Tilmann Seiffert <mail@tilseiffert.de>
2022-05-29 14:01:04 +0200

    refactoring tests

--- 1-hello-world/V1/hello_test.go
+++ 1-hello-world/V1/hello_test.go
@@ -2,33 +2,23 @@ package main
 
 import "testing"
 
-// func TestHello(t *testing.T) {
-// 	got := Hello("Tilmann")
-// 	want := "Hello, Tilmann!"
-
-// 	if got != want {
-// 		t.Errorf("got %q want %q", got, want)
-// 	}
-// }
-
 func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Tilmann")
 		want := "Hello, Tilmann!"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
-
 }
