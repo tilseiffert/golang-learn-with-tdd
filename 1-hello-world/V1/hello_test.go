eb3887c8637a8cdabb9eea2b84523051f345b223
Tilmann Seiffert <mail@tilseiffert.de>
2022-05-29 13:56:40 +0200

    new test for empty string

--- 1-hello-world/V1/hello_test.go
+++ 1-hello-world/V1/hello_test.go
@@ -2,11 +2,33 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello("Tilmann")
-	want := "Hello, Tilmann!"
+// func TestHello(t *testing.T) {
+// 	got := Hello("Tilmann")
+// 	want := "Hello, Tilmann!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+// 	if got != want {
+// 		t.Errorf("got %q want %q", got, want)
+// 	}
+// }
+
+func TestHello(t *testing.T) {
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Tilmann")
+		want := "Hello, Tilmann!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
 }
