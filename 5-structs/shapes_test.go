b32a8c096194837dfbb6809f46a694eb3471dd99
Tilmann Seiffert <mail@tilseiffert.de>
2022-05-30 11:48:41 +0200

    adding shape triangle
    Signed-off-by: Tilmann Seiffert <mail@tilseiffert.de>

--- 5-structs/shapes_test.go
+++ 5-structs/shapes_test.go
@@ -37,6 +37,7 @@ func TestArea(t *testing.T) {
 	}{
 		{"rectangle", Rectangle{12, 6}, 72.0},
 		{"circle", Circle{10}, 314.1592653589793},
+		{"triangle", Triangle{12, 6}, 36.0},
 	}
 
 	for i, tt := range areaTests {
