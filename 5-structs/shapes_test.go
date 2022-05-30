c52af00468585a8f70f2444783a8a86df7495808
Tilmann Seiffert <mail@tilseiffert.de>
2022-05-30 11:46:40 +0200

    adding table test
    Signed-off-by: Tilmann Seiffert <mail@tilseiffert.de>

--- 5-structs/shapes_test.go
+++ 5-structs/shapes_test.go
@@ -1,12 +1,22 @@
 package geometry
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
-func checkFloats(t testing.TB, got float64, want float64) {
+func checkFloats(t testing.TB, got float64, want float64, hint string) {
+
 	t.Helper()
-	if got != want {
-		t.Errorf("got %g want %g", got, want)
+
+	if hint != "" {
+		hint = ", " + hint
 	}
+
+	if got != want {
+		t.Errorf("got %g want %g%s", got, want, hint)
+	}
+
 }
 
 func TestPerimeter(t *testing.T) {
@@ -14,28 +24,24 @@ func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
 	want := 40.0
-	checkFloats(t, got, want)
+	checkFloats(t, got, want, "")
 
 }
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		testname string
+		shape    Shape
+		want     float64
+	}{
+		{"rectangle", Rectangle{12, 6}, 72.0},
+		{"circle", Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for i, tt := range areaTests {
+		got := tt.shape.Area()
+		checkFloats(t, got, tt.want, tt.testname+" (i="+strconv.Itoa(i)+")")
+	}
 
 }
