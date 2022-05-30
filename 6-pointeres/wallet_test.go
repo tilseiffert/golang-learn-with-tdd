be5657dedc460955dc6ea83a9adc126e8c45b451
Tilmann Seiffert <mail@tilseiffert.de>
2022-05-30 17:35:16 +0200

    missing error handling
    Signed-off-by: Tilmann Seiffert <mail@tilseiffert.de>

--- 6-pointeres/wallet_test.go
+++ 6-pointeres/wallet_test.go
@@ -24,6 +24,13 @@ func assertError(t testing.TB, got, want error) {
 	}
 }
 
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
 func TestWallet(t *testing.T) {
 
 	t.Run("deposit", func(t *testing.T) {
@@ -34,7 +41,9 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
