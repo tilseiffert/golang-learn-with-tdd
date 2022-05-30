548af3c239e8ecce6e8241f80c94ed8a2e9c31f4
Tilmann Seiffert <mail@tilseiffert.de>
2022-05-30 11:19:22 +0200

    adding structs
    Signed-off-by: Tilmann Seiffert <mail@tilseiffert.de>

--- 5-structs/shapes_test.go
+++ 5-structs/shapes_test.go
@@ -11,15 +11,16 @@ func checkFloats(t testing.TB, got float64, want float64) {
 
 func TestPerimeter(t *testing.T) {
 
-	//t.Run("check Perimeter", func(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 	checkFloats(t, got, want)
-	//})
+
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
 	want := 72.0
 	checkFloats(t, got, want)
 }
