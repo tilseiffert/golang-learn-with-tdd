9b80a147a2c6ac4e13543ad386b169d7a695cce5
Tilmann Seiffert <mail@tilseiffert.de>
2022-05-30 20:56:53 +0200

    httptest server
    Signed-off-by: Tilmann Seiffert <mail@tilseiffert.de>

--- 12-select/racer_test.go
+++ 12-select/racer_test.go
@@ -1,14 +1,29 @@
 package selectlearn
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.quii.dev"
-	fastURL := "http://tilseiffert.de/linktree"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	assert.Equal(t, fastURL, Racer(slowURL, fastURL))
+
+	slowServer.Close()
+	fastServer.Close()
 }
