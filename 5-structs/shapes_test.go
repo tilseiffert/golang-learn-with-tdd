1f48b89380e361f9bd7504155a4cea597083e98c
Tilmann Seiffert <mail@tilseiffert.de>
2022-05-30 11:56:20 +0200

    refactoring table tests
    Signed-off-by: Tilmann Seiffert <mail@tilseiffert.de>

--- 5-structs/shapes_test.go
+++ 5-structs/shapes_test.go
@@ -1,20 +1,15 @@
 package geometry
 
 import (
-	"strconv"
 	"testing"
 )
 
-func checkFloats(t testing.TB, got float64, want float64, hint string) {
+func checkFloats(t testing.TB, got float64, want float64) {
 
 	t.Helper()
 
-	if hint != "" {
-		hint = ", " + hint
-	}
-
 	if got != want {
-		t.Errorf("got %g want %g%s", got, want, hint)
+		t.Errorf("got %g want %g", got, want)
 	}
 
 }
@@ -24,25 +19,32 @@ func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
 	want := 40.0
-	checkFloats(t, got, want, "")
+	checkFloats(t, got, want)
 
 }
 
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		testname string
-		shape    Shape
-		want     float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{"rectangle", Rectangle{12, 6}, 72.0},
-		{"circle", Circle{10}, 314.1592653589793},
-		{"triangle", Triangle{12, 6}, 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
-	for i, tt := range areaTests {
-		got := tt.shape.Area()
-		checkFloats(t, got, tt.want, tt.testname+" (i="+strconv.Itoa(i)+")")
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				//t.Errorf("got %g want %g for %#v ", got, tt.hasArea, tt.shape)
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
 	}
 
 }
